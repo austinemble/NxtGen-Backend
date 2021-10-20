69876c1d9bac83053fb7a393c86542a7e795ed3e
Austin Emmanuval Noble <austinemmanuval@gmail.com>
2021-10-20 22:37:48 +0530

    change superadmin to admin1

--- src/routes/category.js
+++ src/routes/category.js
@@ -29,7 +29,7 @@ const upload = multer({ storage });
 router.post(
   "/category/create",
   requireSignin,
-  superAdminMiddleware,
+  adminMiddleware,
   upload.single("categoryImage"),
   addCategory
 );
@@ -37,14 +37,14 @@ router.get("/category/getcategory", getCategories);
 router.post(
   "/category/update",
   requireSignin,
-  superAdminMiddleware,
+  adminMiddleware,
   upload.array("categoryImage"),
   updateCategories
 );
 router.post(
   "/category/delete",
   requireSignin,
-  superAdminMiddleware,
+  adminMiddleware,
   deleteCategories
 );
 
